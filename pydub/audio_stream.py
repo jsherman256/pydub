6803af50f2f118b519c97fc4c29c1852a286aeb4
Justin Sherman <jsherman2@gmail.com>
2020-06-15 03:57:54 -0700

    stream now reads the header the same way as segment

--- pydub/audio_stream.py
+++ pydub/audio_stream.py
@@ -1,33 +1,35 @@
-from .audio_segment import extract_wav_headers
+from .audio_segment import WavSubChunk
+import struct
 
 class AudioStream:
-    def read(self, size = None):
-        return self._fd.read(size)
+    # Read a finite number of bytes
+    def read(self, size):
+        tmp = self._fd.read(size)
+        self._pos += size
+        return tmp
 
-    # Read from file descriptor until 'data' header subchunk is found
-    def read_headers(self):
-        self.read(12)  # The size of the RIFF chunk descriptor
-        subchunks = 0
-        buffer = bytearray()
-        while subchunks < 10:
+    def extract_wav_headers(self):
+        self.fastforward(12)  # The size of the RIFF chunk descriptor
+        subchunks = []
+        while len(subchunks) < 10:
             data = self.read(8)
             subchunk_id = data[0:4]
-            print("Subchunk: " + subchunk_id.decode())
-            buffer.extend(data)
+            subchunk_size = struct.unpack_from('<I', data[4:8])[0]
+            subchunks.append(WavSubChunk(subchunk_id, self._pos-8, subchunk_size))
             if subchunk_id == b'data':
                 # 'data' is the last subchunk
-                print("Found data, exiting")
                 break
-
-        return buffer
+            self.fastforward(self._pos + subchunk_size)
+        return subchunks
 
     # Provide forward seek-like behavior to help with loading headers
     def fastforward(self, newpos):
         if self._pos > newpos:
             raise ValueError("Cannot fast-forward to {}; Already at {}".format(newpos, self._pos))
+        self.read(newpos - self._pos)
 
     def __init__(self, stream):
         self._fd = stream
         self._pos = 0
-        headers = extract_wav_headers(self.read_headers())
+        headers = self.extract_wav_headers()
         print(headers)
